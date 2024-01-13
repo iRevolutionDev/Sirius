ed5d5a53be747702a62826b1ec84eb45932f25cc
Revolution <tavinho.daniel@gmail.com>
2024-01-13 00:26:43 -0300

    feat: add window info for basic app

--- examples/basic-app/Main.cpp
+++ examples/basic-app/Main.cpp
@@ -2,5 +2,11 @@
 
 Sirius::window* Sirius::create_window(int, char**)
 {
-    return new window(window_info{});
+    return new window(
+        window_info{
+            .title = "Basic App",
+            .width = 800,
+            .height = 600
+        }
+    );
 }
